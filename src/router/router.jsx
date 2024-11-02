b599fa593ccb4b43c2a6117e08382f38d8d72613
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-02 08:32:02 +0600

    Add listed-books page.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root/Root";
 import PageNotFound from "../components/Body/BodyComponents/PageNotFound";
 import Home from "../components/Body/BodyComponents/Home";
+import ListedBooks from "../components/Body/BodyComponents/ListedBooks";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/listed-books",
+        element: <ListedBooks />,
       },
     ],
   },
