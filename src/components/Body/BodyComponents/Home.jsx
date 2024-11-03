d6d83e41d770f94134ae2930681aa9b367138ad6
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-03 08:12:18 +0600

    Fix banner position.

--- src/components/Body/BodyComponents/Home.jsx
+++ src/components/Body/BodyComponents/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Books from "./Books";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div className="mt-8">
-      <h1 className="font-bold text-center text-3xl md:text-[35px] lg:text-[40px]">
+      <Banner />
+      <h1 className="font-bold text-center text-3xl md:text-[35px] lg:text-[40px] mt-8">
         Books
       </h1>
       <Books />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./HeaderComponents/Navbar";
-import Banner from "../Body/BodyComponents/Banner";
 
 const Header = () => {
   return (
     <div className="px-2">
       <Navbar />
-      <Banner />
     </div>
   );
 };
