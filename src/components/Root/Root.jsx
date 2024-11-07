5b79b962f6c53dc80be07b4c0487e6de98c08101
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-07 19:24:16 +0600

    Fetch books data.

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -3,8 +3,11 @@ import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
 import { BookVibeContext } from "../../Context/BookVibeContext";
+import { useLoaderData } from "react-router-dom";
 
 const Root = () => {
+  const books = useLoaderData();
+  console.log(books);
   return (
     <BookVibeContext.Provider value={{}}>
       <Header />
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -11,6 +11,7 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <PageNotFound />,
+    loader: () => fetch("/BooksDB.json"),
     children: [
       {
         path: "/",
