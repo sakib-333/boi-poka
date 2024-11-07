a281a06b344ac629f92508edd5f2ea0793545fde
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-07 19:15:28 +0600

    Add BookVibeContext.

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
+import { BookVibeContext } from "../../Context/BookVibeContext";
 
 const Root = () => {
   return (
-    <>
+    <BookVibeContext.Provider value={{}}>
       <Header />
       <Body />
       <Footer />
-    </>
+    </BookVibeContext.Provider>
   );
 };
 
