cec72f6d2ccb549c2368b1304f81bbfd878bf950
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-07 21:11:49 +0600

    Add functionality to show all books.

--- src/components/Body/BodyComponents/Book.jsx
+++ src/components/Body/BodyComponents/Book.jsx
@@ -1,40 +1,38 @@
 import React from "react";
 
-import BannerImage from "./assets/banner-image.svg";
 import Star from "./assets/star.svg";
 import { Link } from "react-router-dom";
 
-const Book = () => {
-  const id = 5;
+const Book = ({ book }) => {
   return (
-    <Link to={`/books/id=${id}`}>
+    <Link to={`/books/id=${book.bookId}`}>
       <div className="mx-auto max-w-[375px] w-full border p-4 space-y-3 rounded-xl">
         <div className="bg-base-200 h-[230px] rounded-lg">
           <img
-            className="w-full h-full p-4 box-border"
-            src={BannerImage}
+            className="w-full h-full box-border rounded-xl"
+            src={book.image}
             alt="book"
           />
         </div>
         <div className="flex font-sans font-bold space-x-2">
-          <p className="px-3 py-1 rounded-3xl bg-green-100 text-green-500">
-            Young Adult
-          </p>
-          <p className="px-3 py-1 rounded-3xl bg-green-100 text-green-500">
-            Identity
-          </p>
+          {book.tags.map((tag, indx) => (
+            <p
+              key={`tag_id_${indx}`}
+              className="px-3 py-1 rounded-3xl bg-green-100 text-green-500"
+            >
+              {tag}
+            </p>
+          ))}
         </div>
         <div>
-          <h1 className="font-bold md:text-xl lg:text-2xl">
-            The Catcher in the Rye
-          </h1>
-          <p className="text-sm lg:text-base font-sans">By : Awlad Hossain</p>
+          <h1 className="font-bold md:text-xl lg:text-2xl">{book.bookName}</h1>
+          <p className="text-sm lg:text-base font-sans">By : {book.author}</p>
         </div>
         <div className="border-b-2 border-dashed"></div>
         <div className="flex items-center justify-between">
-          <p>Fiction</p>
+          <p>{book.category}</p>
           <p className="flex items-center font-sans space-x-1">
-            <span>5.00</span> <img src={Star} alt="star" />
+            <span>{book.rating}</span> <img src={Star} alt="star" />
           </p>
         </div>
       </div>
--- src/components/Body/BodyComponents/Books.jsx
+++ src/components/Body/BodyComponents/Books.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Book from "./Book";
+import { BookVibeContext } from "../../../Context/BookVibeContext";
 
 const Books = () => {
+  const { books } = useContext(BookVibeContext);
   return (
     <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-      <Book />
+      {books.map((book) => (
+        <Book key={book.bookId} book={book} />
+      ))}
     </div>
   );
 };
