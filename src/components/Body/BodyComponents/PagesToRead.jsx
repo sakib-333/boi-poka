5282ee6f152699476792e006401a29389ac97174
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-11-07 16:48:19 +0600

    Update pages to read.

--- src/components/Body/BodyComponents/PagesToRead.jsx
+++ src/components/Body/BodyComponents/PagesToRead.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Page A", uv: 4000 },
+  { name: "Page B", uv: 3000 },
+  { name: "Page C", uv: 2000 },
+  { name: "Page D", uv: 2780 },
+  { name: "Page E", uv: 1890 },
+  { name: "Page F", uv: 2390 },
+  { name: "Page G", uv: 3490 },
+];
 
 const PagesToRead = () => {
-  return <div>PagesToRead</div>;
+  return (
+    <div className="mt-4 md:mt-8 lg:mt-16">
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="2 2" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="uv" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default PagesToRead;
